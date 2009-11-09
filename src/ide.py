3ebd1395b514e60cd031f12fdaf16c1bb13671e5
Parham Aram <parham385@hotmail.com>
2009-11-09 19:45:06 +0000

    IDE module, compatible with the new version of the ssmodel

--- src/ide.py
+++ src/ide.py
@@ -6,7 +6,9 @@ interrogate and estimate the model from data.
 # my modules
 import ssmodel, ideBase, bases
 # built-ins
-import numpy, pylab, warnings
+import pylab as pb
+import numpy as np
+import  warnings
 
 class ide:
 	"""class defining an integrodifference equation (ide)
@@ -32,7 +34,7 @@ class ide:
 	Check out the gen_example_x() functions (in this module) for usage examples.
 	"""
 	
-	def __init__(self,kernel,field,space,time,obs_locns,obs_noise_covariance,field_noise_variance,report=0):
+	def __init__(self,kernel,field,space,obs_locns,obs_noise_covariance,field_noise_variance,T,report=0):
 		'''notes:
 		If report is set to 1, a little report about the model is generated.'''
 				
@@ -47,28 +49,28 @@ class ide:
 		self.dimension = field.dimension
 		
 		self.space = space
-		self.time = time
 		self.obs_locns = obs_locns
 		
 		# positive definite covariance matrix check
 		#-------------------numpy.linalg.cholesky(obs_noise_covariance)---------------------
 		# observation location consistency check
+		
+		self.obs_noise_covariance = pb.matrix(obs_noise_covariance)
+		self.field_noise_variance = field_noise_variance
 		assert len(obs_locns) == obs_noise_covariance.shape[0], 'inconsistent observation location size'
 		
-		self.obs_noise_covariance = numpy.matrix(obs_noise_covariance)
-		self.field_noise_variance = field_noise_variance
 		
 		# generate empty ssmodel
 		nx = len(field.bases)
 		ny = len(obs_locns)
 		ntheta = len(kernel.bases)
-		A = numpy.zeros([nx,nx])
-		B = numpy.zeros([nx,0])
-		C = numpy.zeros([ny,nx])
-		Sw = numpy.zeros([nx,nx])
-		Sv = numpy.zeros([ny,ny])
-		T = len(self.time)
-		self.ssmodel = ssmodel.ssmodel(A,B,C,Sw,Sv,T)
+		A = pb.zeros([nx,nx])
+		C = pb.zeros([ny,nx])
+		Sw = pb.zeros([nx,nx])
+		Sv = pb.zeros([ny,ny])
+		x0=pb.zeros([nx,1])
+		self.T=T
+		self.ssmodel = ssmodel.ssmodel(A,C,Sw,Sv,x0)
 		
 		# print a report
 		if report:
@@ -83,7 +85,7 @@ class ide:
 		phi, psi = self.field.bases, self.kernel.bases		
 		nx, ntheta, ny = len(phi), len(psi), len(self.obs_locns)	
 		# theta is the vector containing the weights. 
-		theta = numpy.matrix(self.kernel.weights)
+		theta = pb.matrix(self.kernel.weights)
 		theta.shape = ntheta, 1
 		
 		s = self.obs_locns
@@ -95,7 +97,7 @@ class ide:
 			gamma = [[0 for x in range(nx)] for y in range(nx)]
 			for i in range(nx):
 				for j in range(nx):
-					g = numpy.matrix(numpy.zeros([ntheta,1]))
+					g = pb.matrix(pb.zeros([ntheta,1]))
 					for m in range(ntheta):
 						# form the convolution of phi[j] and psi[m]
 						Phi = phi[j].conv(psi[m])
@@ -106,7 +108,7 @@ class ide:
 			# store the gamma so we don't have to re-generate it each time we do this		
 			self.gamma = gamma
 		# form Psi_theta
-		Psi_theta = numpy.matrix(numpy.zeros([nx,nx]))
+		Psi_theta = pb.matrix(pb.zeros([nx,nx]))
 		for i in range(nx):
 			for j in range(nx):
 				Psi_theta[i,j] = theta.T * gamma[i][j]
@@ -114,7 +116,7 @@ class ide:
 		if hasattr(self,'invPsi_x'):
 			invPsi_x = self.invPsi_x
 		else:		
-			Psi_x = numpy.matrix(numpy.zeros([nx,nx]))
+			Psi_x = pb.matrix(pb.zeros([nx,nx]))
 			for i in range(nx):
 				for j in range(nx):
 					Psi_x[i,j] = phi[i] * phi[j]
@@ -135,47 +137,27 @@ class ide:
 		self.Sw = self.ssmodel.Sw
 		# populate the ssmodel
 		self.ssmodel.Sv = self.obs_noise_covariance
-		self.ssmodel.x0 = numpy.matrix(self.field.weights).T
-		if not(hasattr(self.ssmodel,'time')):
-			self.ssmodel.time = self.time
+		self.ssmodel.x0 = pb.matrix(self.field.weights).T
 		
 	def simulate(self):
 		"""simulates the model across time and space"""
-		self.ssmodel.simulate()
+		X,Y=self.ssmodel.simulate(self.T)
+		return X,Y
 		
-	def gen_surface(self, points=50):
-		"""evaluates the field at the specified number of points across 
-		self.space and at each point self.time"""
-		# unpack
-		min_space, max_space = min(self.space), max(self.space)
-		sim_space = numpy.linspace(min_space,max_space,points)
-		# initialise
-		Z = numpy.zeros([sim_space.shape[0], len(self.time)])
-		# f is the field that whose state we're going to adjust depending on the state
-		# of the ssmodel
-		f = self.field
-		# go through each time point
-		for t in range(len(self.time)):
-			f.weights = [self.ssmodel.X[t][i,0] for i in range(self.ssmodel.nx)]
-			si=0
-			for s in sim_space:		
-				Z[si,t] = f.evaluate(s)
-				si+=1
-		return Z, sim_space
+
 	
-	def estimate_kernel(self):
+	def estimate_kernel(self,X,P,M):
 		"""estimate the ide model's kernel weights from data stored in the ide object"""
 		# unpack
 		nx, na = self.ssmodel.nx, len(self.kernel.weights)
-		X, M, P = self.ssmodel.X, self.ssmodel.M, self.ssmodel.P
 		# form Xi variables
-		Xi_0 = numpy.matrix(numpy.zeros([nx,nx]))
-		Xi_1 = numpy.matrix(numpy.zeros([nx,nx]))
-		for t in range(1,len(self.time)):
+		Xi_0 = pb.matrix(pb.zeros([nx,nx]))
+		Xi_1 = pb.matrix(pb.zeros([nx,nx]))
+		for t in range(1,self.T):
 			Xi_0 += X[t-1] * X[t].T + M[t]
 			Xi_1 += X[t-1] * X[t-1].T + P[t-1]
 		# form Upsilon
-		Upsilon = numpy.matrix(numpy.zeros([na,na]))
+		Upsilon = pb.matrix(pb.zeros([na,na]))
 		temp = self.invPsi_x * self.Sw.I * self.invPsi_x
 		for i in range(nx):
 			for j in range(nx):
@@ -184,7 +166,7 @@ class ide:
 						Upsilon += Xi_1[i,j] * self.gamma[k][j] * temp[k,m] * self.gamma[m][i].T
 		Upsilon.I
 		# form upsilon
-		upsilon = numpy.matrix(numpy.zeros([na,1]))
+		upsilon = pb.matrix(pb.zeros([na,1]))
 		temp = Xi_0 * self.Sw.I * self.invPsi_x
 		for i in range(nx):
 			for j in range(nx):
@@ -194,28 +176,30 @@ class ide:
 		self.kernel.weights = [float(x) for x in weights[:,0]]
 		
 		
-	def estimate_field(self):
-		'''estimates the field of the ide across time'''
-		assert hasattr(self,'obs'), 'observations are not defined'	
-		self.ssmodel.Y = self.obs
-		self.ssmodel.rtssmooth()	
-		
+	def estimate_field(self,Y):
+		'''estimates the field of the ide across time'''	
+		xb, Pb, KStore, M=self.ssmodel.rtssmooth(Y)	
+		return xb, Pb, KStore, M
+
+
 	def em(self,Y):
 		"""estimate the ide's kernel and field weights using an EM algorithm"""
 		# form state soace model
 		self.gen_ssmodel()
 		# generate a random state sequence
-		self.ssmodel.X = [numpy.matrix(numpy.random.rand(self.ssmodel.nx,1)) for t in self.time]
+		X= [pb.matrix(np.random.rand(self.ssmodel.nx,1)) for t in range(self.T)]
+		P=[pb.matrix(pb.zeros((self.ssmodel.nx,self.ssmodel.nx)))]*self.T
+		M=[pb.matrix(pb.zeros((self.ssmodel.nx,self.ssmodel.nx)))]*self.T
+
 		# introduce the observations
-		self.obs = Y
 		# iterate
 		keep_going = 1
 		it_count = 0
-		max_it = 60
+		max_it = 20
 		while keep_going:
-			self.estimate_kernel()
+			self.estimate_kernel(X,P,M)
 			self.gen_ssmodel()
-			self.estimate_field()
+			X, P, KStore, M=self.estimate_field(Y)
 			print it_count, " current estimate: ", self.kernel.weights
 			if it_count == max_it:
 				keep_going = 0
@@ -223,7 +207,7 @@ class ide:
 	
 	def eig(self):
 		'''returns a list of the eigenvalues of the models A matrix'''
-		u,v = numpy.linalg.eig(self.ssmodel.A)
+		u,v = pb.linalg.eig(self.ssmodel.A)
 		return [i for i in u]
 		
 
