5f24286e195b0404766ca474972727ce4af4f209
Parham Aram <parham385@hotmail.com>
2009-12-03 19:55:23 +0000

    Field & plots added

--- src/nonlinide.py
+++ src/nonlinide.py
@@ -3,7 +3,42 @@ import matplotlib.axes3d
 
 
 
+class Field():
+	
+	def __init__(self, weights,centers, widths, dimension,N):
 
+		self.N=N
+		self.weights = weights
+		self.widths=N*widths
+		self.centers=centers
+		self.dimension=dimension
+		
+		self.evaluate = lambda s: sum([w*gaussian(s,cen,wid,dimension) for w,cen,wid, in zip(self.weights,self.centers,self.widths)
+				])
+
+	def __call__(self,s):
+		return float(self.evaluate(s))		
+
+
+	def plot(self,space):
+
+		if self.dimension==1:
+			y=[]
+			z=[]
+			specs=zip(self.weights,self.centers,self.widths)
+			for j in specs:
+				for i in space:
+					z.append(float(j[0]*gaussian(i,j[1],j[2],1)))
+				y.append(z)
+				z=[]
+		[pb.plot(space,y[i],'k') for i in range(self.N)]
+		pb.show()	
+
+		if self.dimension==2:
+			for i in range(len(centers)):
+				for j in range(len(centers)):
+					circle(j*15,i*15,self.width[0])	
+			pb.show()
 
 
 class Kernel():
@@ -26,7 +61,7 @@ class Kernel():
 			y=[]
 			for i in space:
 				y.append(self.__call__(i))		
-			pb.plot(space,y)
+			pb.plot(space,y,'k')
 			pb.show()
 
 		if self.dimension==2:
@@ -64,6 +99,35 @@ def gaussian(s, centre, width,dimension):
 		width = pb.matrix(width)
 		return pb.exp(-(s-centre).T*width.I*(s-centre))
 
+def circle(x,y,r):
+	u=np.linspace(0,2*np.pi,200)
+	x0=np.zeros_like(u)
+	y0=np.zeros_like(u)
+	for i in range(len(u)):
+		x0[i]=r*np.sin(u[i])+x
+		y0[i]=r*np.cos(u[i])+y
+	pylab.plot(x0,y0)
+	pylab.axis([0,90,0,90])	
+
+
+if __name__ == "__main__":
+
+	min_field, max_field, inc = 0, 80, 8
+	f_centers=pb.arange(min_field,max_field+inc,inc)
+	f_widths=[5]
+	f_weights=[1]*11
+	N=11	
+	f=Field(f_weights,f_centers,f_widths,1,11)
+	f_space = pb.linspace(-10,90,500)
+	f.plot(f_space)
+
+	k_centers=pb.array([0,0,0])
+	k_weights = pb.array([.5,-.3,.05])
+	k_widths = pb.array([1.8,36,18**2]) 
+	k_space = pb.linspace(-40,40,500)
+	k=Kernel(k_weights,k_centers,k_widths,1)
+	k.plot(k_space)
+	
 
 
 
