7001a8e5fc60598f64e946c053a0df45f2fffc68
Parham Aram <parham385@hotmail.com>
2009-12-04 15:59:25 +0000

    Calculate disturbance covariance matrix

--- src/nonlinide.py
+++ src/nonlinide.py
@@ -10,27 +10,28 @@ class IDE():
 		self.alpha=alpha
 		self.field_noise_variance=field_noise_variance
 
-	def sim(self,init_field,T,stepsize=10):
+	def sim(self,init_field,T,stepsize=.5):
+		#This is to check, this bit can be done Analitically		
 		Psi_x=(stepsize*sum([self.field.field_bases(s)*self.field.field_bases(s).T for s in self.field.space]))
 		Psi_xinv=Psi_x.I
 		Sw=self.field_noise_variance*Psi_xinv* self.field.field_noise()*Psi_xinv.T
-		#return Sw		
 		Swc=pb.linalg.cholesky(Sw)
-		X=[]		
-		x=init_field
-		X.append(x)
+				
+		#X=[]		
+		#x=init_field
+		#X.append(x)
 
 		
-		for t in range(T):
-			w = pb.random.randn(self.Field.nx,1)
-			sum_s=0
-			for s in space:
-				sum_r=0
-				for r in space:
-					sum_r+=self.field.field_bases(s)*self.act_fun(self.field.field_bases(r).T*x)*self.kernel(abs(s-r))
-				sum_s+=(stepsize**2)*sum_r
-			x=Psi_xinv*sum_s-self.alpha*x+Swc*w
-			X.append(x)					
+		#for t in range(T):
+		#	w = pb.random.randn(self.Field.nx,1)
+		#	sum_s=0
+		#	for s in space:
+		#		sum_r=0
+		#		for r in space:
+		#			sum_r+=self.field.field_bases(s)*self.act_fun(self.field.field_bases(r).T*x)*self.kernel(abs(s-r))
+		#		sum_s+=(stepsize**2)*sum_r
+		#	x=Psi_xinv*sum_s-self.alpha*x+Swc*w
+		#	X.append(x)					
 
 
 
@@ -55,14 +56,21 @@ class Field():
 
 
 
-	def field_noise(self,stepsize=10):
-		sum_s=0
+	def field_noise(self,stepsize=.5):
+		sum_s=[]
 		for s in self.space:
-			sum_r=0
-			for r in self.space:
-				sum_r=self.field_bases(s)*gaussian(abs(s-r),0,10,1)*self.field_bases(r).T
-			sum_s+=(stepsize**2)*sum_r
-		return	pb.matrix(sum_s) #(This is the bit in integral)
+			sum_r=0		
+			for r in self.space:			
+				sum_r+=gaussian(abs(s-r),0,1,1)*self.field_bases(r).T
+			sum_s.append(stepsize*sum_r)
+		sum_int=0
+		for i in range(len(self.space)):
+			sum_int+=self.field_bases(self.space[i])*sum_s[i]
+		return stepsize*sum_int
+
+
+
+
 
 	def plot(self,space):
 
@@ -75,7 +83,7 @@ class Field():
 					z.append(float(j[0]*gaussian(i,j[1],j[2],1)))
 				y.append(z)
 				z=[]
-		[pb.plot(space,y[i],'k') for i in range(self.N)]
+		[pb.plot(space,y[i],'k') for i in range(self.nx)]
 		pb.show()	
 
 		if self.dimension==2:
@@ -156,21 +164,31 @@ def circle(x,y,r):
 
 if __name__ == "__main__":
 
-	min_field, max_field, inc = 0, 80, 8
+	#min_field, max_field, inc = 0, 80, 8
+	min_field, max_field, inc = 1.8, 18, 1.8
+	#f_centers=pb.arange(min_field,max_field+inc,inc)
 	f_centers=pb.arange(min_field,max_field+inc,inc)
-	f_widths=[5]
-	f_weights=[1]*11
-	nx=11
+	#nx=11
+	nx=len(f_centers)
+	#f_widths=[5]
+	f_widths=[1]*nx
+	#f_weights=[1]*11
+	f_weights=[1]*nx
 	T=20	
-	f_space = pb.arange(-10,90,1)
-	f=Field(f_weights,f_centers,f_widths,1,f_space,11)
+	#f_space = pb.arange(-10,90,.1)
+	f_space = pb.arange(-5,25,0.5)
+	f=Field(f_weights,f_centers,f_widths,1,f_space,nx)
 
 	#f.plot(f_space)
 
-	k_centers=pb.array([0,0,0])
-	k_weights = pb.array([.5,-.3,.05])
-	k_widths = pb.array([1.8,36,18**2]) 
-	k_space = pb.linspace(-40,40,1)
+	#k_centers=pb.array([0,0,0])
+	k_centers=[-.5,0,.5]
+	#k_weights = pb.array([.5,-.3,.05])
+	k_weights =[-1,1,.5]
+	#k_widths = pb.array([1.8,36,18**2]) 
+	k_widths = [.1,.1,.1]
+	#k_space = pb.linspace(-40,40,1)
+	k_space = pb.arange(-3,3,0.01)
 	k=Kernel(k_weights,k_centers,k_widths,1)
 	#k.plot(k_space)
 	alpha=.05
